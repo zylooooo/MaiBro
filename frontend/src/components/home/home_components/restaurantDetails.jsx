cc983812be1162a1387b5ceaadd5f8f18615fd87
dasuperhero <leong.kj.sg@gmail.com>
2024-06-11 18:44:15 +0800

    adding search function to home page

--- frontend/src/components/home/home_components/restaurantDetails.jsx
+++ frontend/src/components/home/home_components/restaurantDetails.jsx
@@ -1,19 +1,39 @@
-import React from "react"
+import { React, useState } from 'react'
+import data from "../home_test"
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
-export default function Restaurant(details) {
-    
+export default function Restaurant(detail) {
+    const filteredData = data.filter((el) => {
+        // if no input the return the original
+        // if (!detail.input) {
+        //     return el;
+        // }
+        // //return the item which contains the user input
+        // else {
+        //     return el.text.toLowerCase().includes(detail.input)
+        // }
+        return el.name.toLowerCase().includes(detail.input)
+    })
     return (
+        
         <div className="restaurant">
-            <img 
-                src={`${details.coverImg}`} 
-                className="restaurant-image" 
-            />
-            <div className="restaurant-stats">
-                <AttachMoneyIcon></AttachMoneyIcon>
-                <span>{details.price} • </span>
-                <span className="desc">{details.description}</span>
-            </div>
+            {filteredData.map((item) => (
+                <>
+                <img 
+                    src={`${detail.coverImg}`}
+                    className="restaurant-image" 
+                />
+                <div className="restaurant-stats">
+                    <span className='name'>{item.name}</span>
+                    <div className='restaurant-info'>
+                        <span className='money'><AttachMoneyIcon></AttachMoneyIcon></span>
+                        <span className='price'>{item.price} • </span>
+                        <span className="desc">{item.description}</span>
+                    </div>
+                </div>
+                </>
+            ))}
         </div>
+        
     )
 }
