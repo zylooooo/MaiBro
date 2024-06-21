c894d4d38d6f66673746d6b2b809e00e306d8f7d
zylooooo <zhiyi.loo.2023@scis.smu.edu.sg>
2024-06-21 19:33:40 +0800

    move the comments to the config file

--- backend/config.js
+++ backend/config.js
@@ -24,5 +24,34 @@ const db = getFirestore();
     }
 })();
 
-// Expor the db to be used in other files
+// Export the db to be used in other files
 module.exports = { db };
+
+
+
+/* Firebase terminology:
+   1) Collection: A collection is a group of documents. SQL equivalent of a table. The key is the collecton ID and the value is the documents.
+   2) Document: A document is a set of key-value pairs. SQL equivalent of a row.
+      - Documents cannot contain another document, but they can contain sub collections.
+*/
+
+// Testing the database
+// Creating a new collection and a new document
+// const docRef = db.collection("test").doc("students");
+// docRef.set({
+//     first: "ada",
+//     last: "LoveLace",
+//     age: 30
+// });
+
+// template to create a collection and document in firebase
+// const userRef = db.collection("AvailableOrders").doc("testOrder");
+// userRef.set({
+//     orderID: 123,
+//     orderCompleted: false,
+//     orderAccepted: true,
+//     restaurant: "McDonalds",
+//     buyerID: "buyer123",
+//     broID: "MaiBro",
+//     earnings: 2.00
+// });
