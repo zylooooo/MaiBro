2557eefd6ca1033f7cd7aa12439e8597740815bc
zylooooo <zhiyi.loo.2023@scis.smu.edu.sg>
2024-06-23 16:46:56 +0800

    added check if router is hit

--- backend/routers/loginRouter.js
+++ backend/routers/loginRouter.js
@@ -3,6 +3,11 @@ const { verifyLogin } = require("../controllers/loginController");
 
 const loginRouter = express.Router();
 
+// Check if the route is working
+loginRouter.get("/", (req, res) => {
+    console.log("Login route hit");
+    return res.status(200).json({ message: "Login route hit" });
+});
 loginRouter.post("/login", verifyLogin);
 
 module.exports = loginRouter;
