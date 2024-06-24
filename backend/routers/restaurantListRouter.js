26100161219bd00bf99c553f595dce41cf53e3eb
zylooooo <zhiyi.loo.2023@scis.smu.edu.sg>
2024-06-24 18:38:02 +0800

    router for the restaurant-list

---
+++ backend/routers/restaurantListRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { restaurantList } = require("../controllers/restaurantListController");
+const { db } = require("../config");
+
+const restaurantListRouter = express.Router();
+
+// Check if the route is working
+restaurantListRouter.get("/", async (req, res) => {
+    console.log("Restaurant list route hit!");
+
+    // Testing code to get all the restaurants
+
+    return res.status(200).json({
+        message: "Restaurant list route hit!"
+    });
+});
+restaurantListRouter.post("/", restaurantList);
+
+module.exports = restaurantListRouter;
