58823132c4880fae2269568a942e6e66b935013e
zylooooo <zhiyi.loo.2023@scis.smu.edu.sg>
2024-06-26 23:05:41 +0800

    implemented submit order controller

---
+++ backend/controllers/submitOrderController.js
@@ -0,0 +1,47 @@
+const { db } = require("../config");
+const crypto = require("crypto");
+
+async function submitOrder(req, res) {
+    const {
+        buyerId,
+        earnings,
+        orderAccepted,
+        orderCompleted,
+        restaurant,
+        orderItems,
+        deliveryLocation
+    } = req.body;
+
+    // Update the database with the order details
+    try {
+        const orderId = generateOrderId();
+
+        // Create a new order in the database
+        await db.collection("AvailableOrders").doc(orderId).set({
+            broId: null,
+            buyerId: buyerId,
+            deliveryLocation: deliveryLocation,
+            earnings: earnings,
+            orderAccepted: orderAccepted,
+            orderCompleted: orderCompleted,
+            orderItems: orderItems,
+            restaurant: restaurant
+        });
+
+        return res.status(201).json({
+            message: "Order submitted successfully!"
+        });
+    } catch (error) {
+        console.erro("Error submitting order:", error);
+        return res.status(400).json({
+            error: "Error submitting order!"
+        });
+    }
+}
+
+module.exports = { submitOrder };
+
+// Helper function to generate a random order ID. Returns a 32 character long string
+function generateOrderId() {
+    return crypto.randomBytes(16).toString("hex");
+}
