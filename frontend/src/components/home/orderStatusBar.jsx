fc35e5d260e242eef860ab0deba6933202604c0c
Kai-Tay <kstay.2023@scis.smu.edu.sg>
2024-07-08 18:45:20 +0800

    feat: Added orderStatusBar.jsx page containing order status feature

---
+++ frontend/src/components/home/orderStatusBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css"
+
+export default function OrderStatus() {
+    //LOGIC to obtain accepted orders from backend
+
+    return (
+        <>
+        <div className="orderStatusBar">
+        </div>
+        </>
+    )
+}
