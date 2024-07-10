55abcfdd41a4bee9404879c31f47eeb921c146f8
Kai-Tay <kstay.2023@scis.smu.edu.sg>
2024-07-11 01:24:10 +0800

    feat: Added order status bar (Not Configured)

--- frontend/src/components/home/orderStatusBar.jsx
+++ frontend/src/components/home/orderStatusBar.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import {React, useState} from "react";
+import { useNavigate } from "react-router-dom";
 import "./home.css"
 
 export default function OrderStatus() {
+    const navigate = useNavigate();
     //LOGIC to obtain accepted orders from backend
+    const [isVisible, setIsVisible] = useState(true);
+    const [status, setStatus] = useState("Finding Bros...")
+
+    // Serach for order and check for status
+
+
+    //On Status Bar Click
+    const handleClick = () => {
+        console.log("CLICKED")
+    }
+
 
     return (
         <>
-        <div className="orderStatusBar">
+        <div className={`statusBarDiv ${isVisible ? 'visible' : ''}`} onClick={handleClick} >
+            <div className="orderStatus">
+                <div>Order Status</div>
+                <div style={{fontSize:"12px"}}>{status}</div>
+            </div>
+
         </div>
         </>
     )
