1b0d52f50bc007de93e2c6ef347f64a63992cb62
Kai-Tay <kstay.2023@scis.smu.edu.sg>
2024-07-12 15:14:00 +0800

    feat: Added Component for Order Collected

--- frontend/src/components/delivery/deliveryInfo.jsx
+++ frontend/src/components/delivery/deliveryInfo.jsx
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React , useState } from "react";
 import { ProfileTopBar } from "../common/topTab/topTab";
 import { StandardHeader } from "../common/topTab/topTab";
 import { useLocation } from "react-router-dom";
@@ -8,6 +8,7 @@ import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps"
 import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
 import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
 import RestaurantAddress from "../common/mapAPI/geocoding";
+import WalletIcon from '@mui/icons-material/Wallet';
 
 export function MapDisplay({latitude, longitude}) {
     return(
@@ -26,13 +27,82 @@ export function MapDisplay({latitude, longitude}) {
     )
 }
 
+const DeliveryAccepted = ({info}) => 
+    {
+       const deliveryObj = info;
+       return (
+        <div className="buyerOrderInfoCard">
+                <div className="buyerInfoText">
+                    <div className="restaurantContactInfo">{deliveryObj.restaurant}</div>
+                    <div className='confirmation'>
+                        <RestaurantAddress latitude={deliveryObj.latitude} longitude={deliveryObj.longitude}/>
+                        <div className='delivery-location'>
+                        <LocalShippingOutlinedIcon />
+                        <div style={{paddingLeft:"10px"}}>{deliveryObj.deliveryLocation}</div>
+                        </div>
+                        <div className='order'>
+                        <ArticleOutlinedIcon />
+                        <div style={{paddingLeft:"10px"}}>{deliveryObj.orderItems}</div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div>
+                    <Button disableRipple fullWidth variant='contained' className='confirm-button'
+                    style={{borderRadius: "25px", fontSize:"0.8em",backgroundColor:"#C6252E",height:"3.5em",textTransform:"none",fontWeight:"1000"}}
+                    onClick={""}>
+                    Arrived at Restaurant
+                    </Button>
+                </div>
+            </div>
+       )
+    }
+    
+
+const DeliveryCollected = ({info}) => 
+    {
+       const deliveryObj = info;
+       return (
+        <div className="buyerOrderInfoCard">
+                <div className="buyerInfoText">
+                    <div className="buyerContactInfo">
+                        <LocalShippingOutlinedIcon/>
+                        <div style={{paddingLeft:"10px"}}>{deliveryObj.deliveryLocation}</div>
+                    </div>
+                    <div className='confirmation'>
+                        <div className='order'>
+                        <ArticleOutlinedIcon />
+                        <div style={{paddingLeft:"10px"}}>{deliveryObj.orderItems}</div>
+                        </div>
+                        <div className="order">
+                        <WalletIcon />
+                            <div style={{paddingLeft:"10px"}}>${deliveryObj.earnings}</div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div>
+                    <Button disableRipple fullWidth variant='contained' className='confirm-button'
+                    style={{borderRadius: "25px", fontSize:"0.8em",backgroundColor:"#C6252E",height:"3.5em",textTransform:"none",fontWeight:"1000"}}
+                    onClick={""}>
+                    Order Received
+                    </Button>
+                </div>
+            </div>
+       )
+    }
+
+
 export default function DeliveryInfo() {
     const location = useLocation();
+    const [collected, setCollected] = useState(true)
     const deliveryObj = location.state.delivery[0];
 
     //Chat Button
 
     //Arrived at Restaurant Button -> Change Status
+    // Needa figure out how to store this HAHAHAH
+
 
     return (
         <>
@@ -52,30 +122,8 @@ export default function DeliveryInfo() {
                 </div>
             </div>
             <MapDisplay latitude={deliveryObj.latitude} longitude={deliveryObj.longitude}/>
-            <div className="buyerOrderInfoCard">
-                <div className="buyerInfoText">
-                    <div className="restaurantContactInfo">{deliveryObj.restaurant}</div>
-                    <div className='confirmation'>
-                        <RestaurantAddress latitude={deliveryObj.latitude} longitude={deliveryObj.longitude}/>
-                        <div className='delivery-location'>
-                        <LocalShippingOutlinedIcon />
-                        <div style={{paddingLeft:"10px"}}>{deliveryObj.deliveryLocation}</div>
-                        </div>
-                        <div className='order'>
-                        <ArticleOutlinedIcon />
-                        <div style={{paddingLeft:"10px"}}>{deliveryObj.orderItems}</div>
-                        </div>
-                        
-                    </div>
-                </div>
-                <div className='cancel-button'>
-                    <Button disableRipple fullWidth variant='contained' className='confirm-button'
-                    style={{borderRadius: "25px", fontSize:"0.8em",backgroundColor:"#C6252E",height:"3.5em",textTransform:"none",fontWeight:"1000"}}
-                    onClick={""}>
-                    Arrived at Restaurant
-                    </Button>
-                </div>
-            </div>
+            {/* INSERT IF ELSE CONDITION HERE */}
+            {collected ? <DeliveryCollected info={deliveryObj}/> : <DeliveryAccepted info={deliveryObj}/>}
         <div>
             <BottomTab value="Delivery"></BottomTab>
         </div>
