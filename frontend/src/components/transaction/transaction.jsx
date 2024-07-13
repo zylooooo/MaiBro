6ea007d26eaff106fd0bcb4b7395f6c2e8ff0445
gloriaphua <gloriaphua15@gmail.com>
2024-07-14 01:52:32 +0800

    done w transatction

--- frontend/src/components/transaction/transaction.jsx
+++ frontend/src/components/transaction/transaction.jsx
@@ -1,15 +1,101 @@
 import React from "react"
 import BottomTab from "../common/bottomTab/bottomTab"
 import { ProfileTopBar, StandardHeader } from "../common/topTab/topTab"
+import transactionlist from "./transactionListings.jsx"
 
-export default function Transaction() {
-    return (
+
+var transaction = transactionlist["Listings"];
+
+
+export function Transaction() {
+  return (
+    <>
+    <div>
+<ProfileTopBar />
+<StandardHeader headerName="History"/>
+        
+         </div>
+    {transaction.map(function(data) {
+      var completedOrPending;
+      if(data.orderCompleted){
+        completedOrPending = "Completed";
+      }else{
+        completedOrPending = "Pending";
+      }
+      return (
         <div>
-            <ProfileTopBar />
-            <StandardHeader headerName="History"/>
-            <div>
-                <BottomTab value="Transaction"></BottomTab>
-            </div>
+        {data.restaurantName} {data.orderDate} {data.orderTime} {completedOrPending}
+
         </div>
-    )
+        
+      )
+    })}
+         <div>
+
+        <BottomTab value="Transaction"></BottomTab>
+
+         </div>
+    </>
+
+  )
 }
+
+export default Transaction;
+
+// export default function Transaction() {
+//     var transaction = transactionlist["Listings"]
+
+//     return (
+//         <>
+//         <div>
+//             <ProfileTopBar />
+//             <StandardHeader headerName="History"/>
+        
+//         </div>
+        
+//         <div>
+
+//         </div>
+        
+//         <div>
+
+//             <BottomTab value="Transaction"></BottomTab>
+
+//         </div>
+//         </>
+        
+//     )
+// }
+
+// export default function Transaction() {
+//     var transaction = transactionlist["Listings"]
+
+//     return (
+//         <>
+//         <div>
+//             <ProfileTopBar />
+//             <StandardHeader headerName="History"/>
+        
+//         </div>
+        
+//         <div>
+//         {
+//             transaction.map(function(data)){
+//                 return(
+//                     <div>
+//                     {data.restaurantName}
+//                     </div>
+//                 )
+            
+//             })}
+//         </div>
+        
+//         <div>
+
+//             <BottomTab value="Transaction"></BottomTab>
+
+//         </div>
+//         </>
+        
+//     )
+// }
